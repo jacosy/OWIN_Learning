5e06d72aa81f3f12e97e7f6824d505515f2dfc2f
Longoria.You <longoria.you@gmail.com>
2016-10-22 00:42:23 +0800

    Building a Simple OWIN Pipeline

---
+++ OwinDemo/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Diagnostics;
+
+[assembly: OwinStartup(typeof(OwinDemo.Startup))]
+
+namespace OwinDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 如需如何設定應用程式的詳細資訊，請參閱  http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Use(async (context, next) =>
+            {
+                Debug.WriteLine($"Incoming Request: {context.Request.Path.Value}");
+                await next();
+                Debug.WriteLine($"Outcoming Response: {context.Response.ContentType}");
+            });
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
